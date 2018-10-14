7f3033152a140d04c3f74fab8721d5560cd0238c
ivzhang1 <izhang1@stuy.edu>
2018-10-14 00:42:24 -0400

    gotta edit add alpha

--- song_node.c
+++ song_node.c
@@ -37,22 +37,33 @@ struct song_node * free_list(struct song_node * l_list){
 }
 
 struct song_node * insert_node_alpha(struct song_node * l_list, char *_name, char *_artist){
+  
+  if(strcmp(_artist, l_list->artist) < 0 && strcmp(_name, l_list->name) < 0){
+    printf("RRUN");
+    return insert_front(l_list, _name, _artist);
+  }
+
+  
   struct song_node *new =  malloc(sizeof(struct song_node));
   strcpy(new->name, _name);
   strcpy(new->artist, _artist);
   struct song_node *curr = l_list;
   
-  while(curr->next && strcmp(_artist, curr->next->artist) < 0){
+  while(curr->next && strcmp(_artist, curr->artist) > 0){
     curr = curr->next;
   }
 
-  while(curr->next && strcmp(_artist, curr->next->artist) == 0 && strcmp(_name, curr->next->name) < 0){
+  while(curr->next && strcmp(_artist, curr->artist) == 0 && strcmp(_name, curr->name) > 0){
     curr=curr->next;
   }
-  new->next = curr;
-  if(&curr == &l_list){
-    return new;
+
+  if(curr->next == NULL){
+    curr->next = new;
   }
-  return l_list;
+  else{
+    new->next = curr;
+  }
+
+    return l_list;
   
 }
--- test.c
+++ test.c
@@ -6,23 +6,27 @@ int main(){
 
   char *s_1 = "no women no cry";
   char *a_1 = "bob marley";
-  struct song_node * p_test_1 = insert_front(0, s_1, a_1);
+  struct song_node * p_test_1 =  insert_front(0, s_1, a_1);
 
   char *s_2 = "blackbird";
   char *a_2 = "beetles";
-  p_test_1 = insert_front(p_test_1, s_2, a_2);
+  p_test_1 =  insert_node_alpha(p_test_1, s_2, a_2);
+  
+  char *s_21 = "dolly";
+  char *a_21 = "beetles";
+  p_test_1 =  insert_node_alpha(p_test_1, s_21, a_21);
 
   char *s_3 = "september";
   char *a_3 = "earth wind fire";
-  p_test_1 = insert_front(p_test_1, s_3, a_3);
+  p_test_1 =  insert_node_alpha(p_test_1, s_3, a_3);
 
   char *s_4 = "africa";
   char *a_4 = "toto";
-  p_test_1 = insert_front(p_test_1, s_4, a_4);
+  p_test_1 =  insert_node_alpha(p_test_1, s_4, a_4);
 
   char *s_5 = "crys from CS kids";
   char *a_5 = "debugging";  
-  p_test_1 = insert_front(p_test_1, s_5, a_5);
+  p_test_1 = insert_node_alpha(p_test_1, s_5, a_5);
 
 
   printf("LINKED LIST TESTS\n====================================\n\n");
@@ -34,6 +38,8 @@ int main(){
   print_node(p_test_1);
   printf("\n====================================\n\n");
 
+
+  
   //printf("CLEARING Linked List\n");
   //p_test_1 = free_list(p_test_1);
 
